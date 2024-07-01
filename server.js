7a3e61800654a3c5f39693e91dc721f71ca8b0bf
f-zahra <eva-200@hotmail.fr>
2024-07-01 13:55:44 -0400

    installed and implemented morgan logger middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -7,9 +8,17 @@ dotenv.config({ path: "./config/config.env" });
 //create instance of express app
 const app = express();
 
+//Dev logging middleware
+if (process.env.NODE_ENV === "developement") {
+  app.use(morgan("dev"));
+}
+
 //import routes
 const instructorsRoutes = require("./routes/instructors");
+
 app.use("/api/v1/instructors", instructorsRoutes);
+
+//PORT var
 const PORT = process.env.PORT || 8080;
 
 //start server and listen to port
