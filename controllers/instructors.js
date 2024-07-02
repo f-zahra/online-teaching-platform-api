e79c99fcce449f31c148555b474987575bc99658
f-zahra <eva-200@hotmail.fr>
2024-07-02 12:54:14 -0400

    get, update and delete instructor by id

--- controllers/instructors.js
+++ controllers/instructors.js
@@ -1,4 +1,5 @@
 const Instructor = require("../models/Instructor");
+const ErrorResponse = require("../utils/errorResponse");
 
 //get all
 exports.getInstructors = async (req, res) => {
@@ -6,29 +7,85 @@ exports.getInstructors = async (req, res) => {
 
   res.status(200).json({
     success: true,
-    count: instructors.length.toFixed,
+    count: instructors.length,
     data: instructors,
   });
 };
+
 //get by id
-//validate id(correct and incorrect format)
+exports.getInstructorById = async (req, res, next) => {
+  try {
+    const instructor = await Instructor.findById(req.params.id);
+
+    if (!instructor) {
+      return next(
+        new ErrorResponse(
+          400,
+          `Instructor with id ${req.params.id}was not found`
+        )
+      );
+    }
+    res.status(200).json({
+      success: true,
+
+      data: instructor,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 //create a instructor
-exports.addInstructor = async (req, res) => {
+exports.addInstructor = async (req, res, next) => {
   //insert data
   try {
     const instructor = await Instructor.create(req.body);
 
     res.status(201).json({ success: true, data: instructor });
   } catch (error) {
-    res.status(400).json({ success: false });
+    next(error);
   }
 };
 
-exports.updateInstructor = (req, res) => {
-  //TODO update instructor data
+exports.updateInstructor = async (req, res, next) => {
+  try {
+    const instructor = await Instructor.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!instructor) {
+      return next(
+        new ErrorResponse(
+          400,
+          `Instructor with id ${req.params.id}was not found`
+        )
+      );
+    }
+    res.status(200).json({ success: true, data: instructor });
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.deleteInstructor = (req, res) => {
-  //TODO delete instructor by id
+exports.deleteInstructor = async (req, res, next) => {
+  try {
+    const instructor = await Instructor.findByIdAndDelete(req.params.id);
+
+    if (!instructor) {
+      return next(
+        new ErrorResponse(
+          400,
+          `Instructor with id ${req.params.id}was not found`
+        )
+      );
+    }
+    res.status(200).json({ success: true, data: {} });
+  } catch (error) {
+    next(error);
+  }
 };
