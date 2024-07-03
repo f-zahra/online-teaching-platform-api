5a0db5b1fce5071592963cc0c41e159284c020f7
f-zahra <eva-200@hotmail.fr>
2024-07-02 20:50:50 -0400

    add pagination to queries

--- controllers/instructors.js
+++ controllers/instructors.js
@@ -7,7 +7,7 @@ exports.getInstructors = asyncHandler(async (req, res) => {
   //fields to exclude
   let query;
   const reqQuery = { ...req.query };
-  const removeFields = ["select, sort"];
+  const removeFields = ["select", "sort", "page", "limit"];
   removeFields.forEach((param) => delete reqQuery[param]);
 
   let queryStr = JSON.stringify(reqQuery);
@@ -34,12 +34,32 @@ exports.getInstructors = asyncHandler(async (req, res) => {
     query = query.sort({ averageRating: -1 });
   }
 
+  //pagination
+  const page = parseInt(req.query.page) || 1; // Current page number, default is 1
+  const limit = parseInt(req.query.limit) || 10; // Number of items per page, default is 10
+  const startIndex = (page - 1) * limit; // Calculate number of documents to skip (ex:  20 documents to get to page 3 )
+  const endIndex = page * limit;
+  const total = await Instructor.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   //execute query
   const instructors = await query;
+
+  //creat pagination object with prev and next and limit per page
+
+  const pagination = {};
+  if (endIndex < total) {
+    pagination.next = { page: page + 1, limit };
+  }
+  if (startIndex > 0) {
+    pagination.prev = { page: page - 1, limit };
+  }
 
   res.status(200).json({
     success: true,
     count: instructors.length,
+    pagination,
     data: instructors,
   });
 });
