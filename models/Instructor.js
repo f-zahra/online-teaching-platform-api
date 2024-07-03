141a0531a148ed31e5235d15077b03d457d2114a
f-zahra <eva-200@hotmail.fr>
2024-07-03 12:17:11 -0400

    add course virtuals to instructor  document

--- models/Instructor.js
+++ models/Instructor.js
@@ -1,103 +1,118 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 
-const InstructorSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
-    trim: true,
-    maxlength: [50, "Name cannot be more than 50 characters"],
-  },
-  slug: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: [true, "Please add an email"],
-    unique: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please add a valid email",
-    ],
-  },
-  password: {
-    type: String,
-    required: [true, "Please add a password"],
-    minlength: [6, "Password must be at least 6 characters"],
-    select: false, // Do not return the password field in queries
-  },
-  bio: {
-    type: String,
-    trim: true,
-    maxlength: [500, "Bio cannot be more than 500 characters"],
-  },
-  expertise: {
-    type: [String],
-    required: true,
-    enum: [
-      "Web Development",
-      "Mobile Development",
-      "Data Science",
-      "Machine Learning",
-      "Business",
-      "UI/UX",
-      "Other",
-    ],
-  },
-  photo: {
-    type: String,
-    default: "no-photo.jpg",
-  },
-  website: {
-    type: String,
-    match: [
-      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-      "Please add a valid URL with HTTP or HTTPS",
-    ],
-  },
-  social: {
-    twitter: {
+const InstructorSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
+      required: [true, "Please add a name"],
+      trim: true,
+      maxlength: [50, "Name cannot be more than 50 characters"],
+    },
+    slug: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
       match: [
-        /https?:\/\/(www\.)?twitter\.com\/[A-Za-z0-9_]{1,15}/,
-        "Please add a valid Twitter URL",
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
       ],
     },
-    linkedin: {
+    password: {
       type: String,
-      match: [
-        /https?:\/\/(www\.)?linkedin\.com\/in\/[A-Za-z0-9_-]{1,30}/,
-        "Please add a valid LinkedIn URL",
+      required: [true, "Please add a password"],
+      minlength: [6, "Password must be at least 6 characters"],
+      select: false, // Do not return the password field in queries
+    },
+    bio: {
+      type: String,
+      trim: true,
+      maxlength: [500, "Bio cannot be more than 500 characters"],
+    },
+    expertise: {
+      type: [String],
+      required: true,
+      enum: [
+        "Web Development",
+        "Mobile Development",
+        "Data Science",
+        "Machine Learning",
+        "Business",
+        "UI/UX",
+        "Other",
       ],
     },
-    github: {
+    photo: {
+      type: String,
+      default: "no-photo.jpg",
+    },
+    website: {
       type: String,
       match: [
-        /https?:\/\/(www\.)?github\.com\/[A-Za-z0-9_-]{1,39}/,
-        "Please add a valid GitHub URL",
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        "Please add a valid URL with HTTP or HTTPS",
       ],
     },
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  courses: [
-    {
-      type: mongoose.Schema.ObjectId,
-      ref: "Course",
+    social: {
+      twitter: {
+        type: String,
+        match: [
+          /https?:\/\/(www\.)?twitter\.com\/[A-Za-z0-9_]{1,15}/,
+          "Please add a valid Twitter URL",
+        ],
+      },
+      linkedin: {
+        type: String,
+        match: [
+          /https?:\/\/(www\.)?linkedin\.com\/in\/[A-Za-z0-9_-]{1,30}/,
+          "Please add a valid LinkedIn URL",
+        ],
+      },
+      github: {
+        type: String,
+        match: [
+          /https?:\/\/(www\.)?github\.com\/[A-Za-z0-9_-]{1,39}/,
+          "Please add a valid GitHub URL",
+        ],
+      },
     },
-  ],
-  averageRating: {
-    type: Number,
-    min: [1, "Rating must be at least 1"],
-    max: [10, "Rating can not be more than 10"],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    averageRating: {
+      type: Number,
+      min: [1, "Rating must be at least 1"],
+      max: [10, "Rating can not be more than 10"],
+    },
   },
-});
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
 
 InstructorSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
+});
+
+//cascade delete courses if an instructor is deleted
+InstructorSchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function (next) {
+    await this.model("Course").deleteMany({ instructor: this._id });
+    next();
+  }
+);
+
+//courses virtuals
+InstructorSchema.virtual("courses", {
+  ref: "Course",
+  localField: "_id",
+  foreignField: "instructor",
+  justOne: false,
 });
 
 module.exports = mongoose.model("Instructor", InstructorSchema);
