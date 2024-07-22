3a75ebf654f58d9a20acf730b2421c99b0549fa5
f-zahra <eva-200@hotmail.fr>
2024-07-22 15:08:25 -0400

    updated user model

--- models/User.js
+++ models/User.js
@@ -2,8 +2,30 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 100,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    min: 5,
+    max: 255,
+  },
+  role: {
+    type: String,
+    enum: ["user, publisher"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+    max: 100,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
