4171f9d17475afa8f3d4d6a0eb2aa077a585534f
f-zahra <eva-200@hotmail.fr>
2024-07-22 16:24:57 -0400

    hash password

--- models/User.js
+++ models/User.js
@@ -28,5 +28,15 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// Hash the password before saving
+//used for both creating new users (registration) and updating existing users.
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
 const User = mongoose.model("User", userSchema);
 module.exports = User;
